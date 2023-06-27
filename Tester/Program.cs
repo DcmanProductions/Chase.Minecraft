666b2206c185fcf219ec3fdef0fb762738945ca9
Drew Chase <dcmanproductions@gmail.com>
2023-06-27 05:04:06 -0400

    Add project files.

---
+++ Tester/Program.cs
@@ -0,0 +1,19 @@
+﻿using Chase.Minecraft;
+using System.Text.Json;
+
+namespace Tester
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (MCAuth.Login("drew_cat@live.com", "zRr5x743ySLPnsF", out MCUser user))
+            {
+                Console.WriteLine(JsonSerializer.Serialize(user));
+            }else
+            {
+                Console.WriteLine("INVALID");
+            }
+        }
+    }
+}
